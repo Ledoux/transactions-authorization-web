42c61cb3ba6c2aa2b46430aad89392c9419fb1ca
erwanledoux <lawanledoux@gmail.com>
2017-09-11 01:36:58 +0200

    pure DashboardPage

--- src/react/pages/DashboardPage.js
+++ src/react/pages/DashboardPage.js
@@ -1,138 +1,56 @@
 import classnames from 'classnames'
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { push } from 'react-router-redux'
-import { getLocationSearch,
-  getLocationSearchString,
-  showModalHome
-} from 'transactions-interface-state'
-import { setAuthorizationSelectedMode } from 'transactions-authorization-state'
+import React from 'react'
+import { DashboardPage as withState } from 'transactions-authorization-state'
 
 import ModesBar from '../components/ModesBar'
 import ModesDropdown from '../components/ModesDropdown'
 
-class DashboardPage extends Component {
-  constructor () {
-    super ()
-    this.handleBackHome = this._handleBackHome.bind(this)
-  }
-  _handleBackHome (prevProps) {
-    const { availableModes,
-      modalViewer,
-      pathName,
-      push,
-      selectedMode,
-      setAuthorizationSelectedMode,
-      showModalHome,
-      visibleModes
-    } = this.props
-    // special modals when go back home
-    if (window.location.pathname === pathName) {
-      const search = getLocationSearch(window.location.search)
-      const ModalHomeComponent = search && search.modal &&
-        modalViewer && modalViewer[search.modal]
-      if (ModalHomeComponent) {
-        showModalHome(search.modal, search, search.selectedModeName)
-        return
-        // at initial time
-      } else if (selectedMode && selectedMode.name && (!search ||
-        !search.selectedModeName)) {
-        const nextSearch = getLocationSearchString(
-          Object.assign({}, search, { selectedModeName: selectedMode.name }))
-        push({ search: nextSearch })
-      } else if (search && search.selectedModeName) {
-        const nextSelectedMode = availableModes && availableModes.find(({name}) =>
-          name === search.selectedModeName)
-        if (nextSelectedMode) {
-          setAuthorizationSelectedMode(nextSelectedMode)
-        } else if (visibleModes && visibleModes[0]) {
-          const nextSearch = getLocationSearchString(
-            Object.assign({}, search, { selectedModeName: visibleModes[0].name }))
-          push({ search: nextSearch })
-        }
-      } else if (!selectedMode && visibleModes && visibleModes[0]) {
-        setAuthorizationSelectedMode(visibleModes[0])
-      }
-    }
-  }
-  componentDidMount () {
-    this.handleBackHome()
-  }
-  componentDidUpdate (prevProps) {
-    this.handleBackHome(prevProps)
-  }
-  render () {
-    const { api,
-      dashboardViewer,
-      DefaultDashboardComponent,
-      firstName,
-      selectedMode,
-      visibleModes
-    } = this.props
-    return (
-      <main className='page dashboard-page main'>
-        <div className='dashboard-page__modes'>
-          <div className='dashboard-page__modes__bar'>
-            {
-              (visibleModes && visibleModes.length > 1) &&
-              <ModesBar modes={visibleModes}
-                selectedMode={selectedMode}
-              />
-            }
-          </div>
-          <div className='dashboard-page__modes__dropdown'>
-            {
-              (visibleModes && visibleModes.length > 1) &&
-              <ModesDropdown modes={visibleModes}
-                selectedMode={selectedMode}
-              />
-            }
-          </div>
-        </div>
-        <div className='dashboard-page__content'>
+const DashboardPage = ({ api,
+  dashboardViewer,
+  DefaultDashboardComponent,
+  firstName,
+  selectedMode,
+  visibleModes
+}) => {
+  return (
+    <main className='page dashboard-page main'>
+      <div className='dashboard-page__modes'>
+        <div className='dashboard-page__modes__bar'>
           {
-            visibleModes && visibleModes.map(({ name }, index) => {
-              const DashboardComponent = dashboardViewer && dashboardViewer[name]
-              const isHidden = name !== (selectedMode && selectedMode.name)
-              return DashboardComponent && (
-                <div className={classnames('dashboard-page__content__dashboard', {
-                  'dashboard-page__content__dashboard--hidden': isHidden
-                })}
-                  key={index}
-                >
-                  <DashboardComponent api={api} />
-                </div>
-              )
-            })
+            (visibleModes && visibleModes.length > 1) &&
+            <ModesBar modes={visibleModes}
+              selectedMode={selectedMode}
+            />
           }
         </div>
-      </main>
-    )
-  }
+        <div className='dashboard-page__modes__dropdown'>
+          {
+            (visibleModes && visibleModes.length > 1) &&
+            <ModesDropdown modes={visibleModes}
+              selectedMode={selectedMode}
+            />
+          }
+        </div>
+      </div>
+      <div className='dashboard-page__content'>
+        {
+          visibleModes && visibleModes.map(({ name }, index) => {
+            const DashboardComponent = dashboardViewer && dashboardViewer[name]
+            const isHidden = name !== (selectedMode && selectedMode.name)
+            return DashboardComponent && (
+              <div className={classnames('dashboard-page__content__dashboard', {
+                'dashboard-page__content__dashboard--hidden': isHidden
+              })}
+                key={index}
+              >
+                <DashboardComponent api={api} />
+              </div>
+            )
+          })
+        }
+      </div>
+    </main>
+  )
 }
 
-DashboardPage.defaultProps = {
-  excludedModeNames: ['guest', 'user', 'active'],
-  pathName: '/dashboard'
-}
-
-function mapStateToProps ({ authorization: { availableModes,
-    selectedMode,
-    visibleModes
-  },
-  dashboardViewer,
-  modalViewer,
-  user: { firstName }
-}) {
-  return { availableModes,
-    dashboardViewer,
-    firstName,
-    modalViewer,
-    selectedMode,
-    visibleModes
-  }
-}
-export default connect(mapStateToProps, { setAuthorizationSelectedMode,
-  showModalHome,
-  push
-})(DashboardPage)
+export default withState(DashboardPage)
