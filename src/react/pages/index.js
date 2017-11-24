96ac705daf39a3ee79e319468182d04bb7a58ed0
erwanledoux <lawanledoux@gmail.com>
2017-11-24 01:05:31 +0100

    removed ContentPage

--- src/react/pages/index.js
+++ src/react/pages/index.js
@@ -1,15 +1,17 @@
+import { ContentPage } from 'transactions-cms-web'
 import { withForcedProps } from 'transactions-redux-react'
 import { SigninPage as _SigninPage,
   SignupPage as _SignupPage
 } from 'transactions-user-web'
 
+import DashboardPage from './DashboardPage'
+
 const returnToExtraProps = { returnTo: '/dashboard' }
 const SigninPage = withForcedProps(returnToExtraProps)(_SigninPage)
 const SignupPage = withForcedProps(returnToExtraProps)(_SignupPage)
 
-import DashboardPage from './DashboardPage'
-
-export { DashboardPage,
+export { ContentPage,
+  DashboardPage,
   SigninPage,
   SignupPage
 }
